6ac6266c7a7ca6544bce3c6b34d231d36ed856ff
82deutschmark <38813642-82deutschmark@users.noreply.replit.com>
2025-02-24 03:19:19 +0000

    Agent query: Can you check if the /upload page is now accessible and working?
    Fix: Add upload page route
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/fb0f0ac6-e9cd-4aca-8ffb-271cd0ae97b8/9986693e-8a13-4745-8c8d-fed8f593147f.jpg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,7 @@ import ReviewPage from "@/pages/create/review";
 import StoryGenerationPage from "@/pages/create/story-generation";
 import YorkieSelector from "@/pages/create/yorkie-selector";
 import DebugPage from "@/pages/debug";
+import UploadPage from "@/pages/upload";
 import AppearancePage from "@/pages/create/appearance";
 import CharacterPage from "@/pages/create/character";
 import StoryPage from "@/pages/create/story";
@@ -18,6 +19,7 @@ function Router() {
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/home" component={Home} />
+      <Route path="/upload" component={UploadPage} />
       <Route path="/create/appearance" component={AppearancePage} />
       <Route path="/create/character" component={CharacterPage} />  
       <Route path="/create/story" component={StoryPage} />
@@ -25,7 +27,6 @@ function Router() {
       <Route path="/create/review" component={ReviewPage} />
       <Route path="/create/story-generation" component={StoryGenerationPage} />
       <Route path="/create/yorkie-selector" component={YorkieSelector} />
-      <Route path="/debug" component={DebugPage} />
       <Route path="/debug" component={DebugPage} />
       <Route component={NotFound} />
     </Switch>
